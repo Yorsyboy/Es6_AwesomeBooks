cdbeedad6814ec8e448a9219e94155c4a0726eb7
yorsyboy <adekunletaiwo43@yahoo.com>
2022-04-25 07:44:28 +0100

    crreate modules and refactore codes to es6 and arrow function

---
+++ modules/library.js
@@ -0,0 +1,53 @@
+import storageAvailable from './storageAvailable.js';
+
+export default class Library {
+    // class library consist of list of books
+    books = [];
+
+    constructor() {
+      this.books = [];
+      // Load intial book stored
+      const localBooksData = localStorage.getItem('books');
+      if (localBooksData) {
+        this.books = JSON.parse(localBooksData);
+      }
+    }
+
+    // check if a book exists
+    bookExists(book) {
+      for (let i = 0; i < this.books.length; i += 1) {
+        if (this.books[i].title === book.title && this.books[i].author === book.author) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    // add a new book
+    addBook(book) {
+      if (!this.bookExists(book)) {
+        this.books.push(book);
+        this.updateLocalStorage();
+        return book;
+      }
+      alert('The Book and Author exist');
+    }
+
+    // Remove a book from the list of lists
+    removeBook(book) {
+      for (let i = 0; i < this.books.length; i += 1) {
+        if (this.books[i].title === book.title && this.books[i].author === book.author) {
+          this.books.splice(i, 1);
+          this.updateLocalStorage();
+          return;
+        }
+      }
+    }
+
+    // update locastorage
+    updateLocalStorage() {
+      if (storageAvailable('localStorage')) {
+        localStorage.setItem('books', JSON.stringify(this.books));
+      }
+    }
+}
